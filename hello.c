5e09f20f49380c01c524e80f91634db4d463ead3
Joshua Dotto <djoshua@huskers.unl.edu>
2020-08-21 02:21:34 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,21 @@
+/**
+ * Author: Joshua Dotto
+ *         djoshua@huskers.unl.edu
+ * Date: 2020/08/20
+ *
+ * Hack 1.0 assignment
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+   printf("Joshua Dotto\n");
+
+
+
+
+
+   return 0;
+}
